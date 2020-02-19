f5b4ce2fb7ca4469514d7e4cc47ac2a8f88430a5
Abraham Zilberblat <39259098+AZilberblat@users.noreply.github.com>
2020-02-19 18:37:34 +0200

    Updates

---
+++ precache-manifest.bb9b342bf9db290f3bc797edebaeacc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa222eed9a348a40110e5e39368c43bf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "921da07215354c3b990a",
+    "url": "/monsters-rolodex/static/css/main.3904a2d3.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921da07215354c3b990a",
+    "url": "/monsters-rolodex/static/js/main.b662ca02.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
